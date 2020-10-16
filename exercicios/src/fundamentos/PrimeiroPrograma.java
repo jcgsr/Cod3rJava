56b3ac09f051942d0a5f103799509e3322d71423
jcgsr <cabralguerra@gmail.com>
2020-10-16 00:48:20 -0300

    first commit

---
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,12 @@
+// 15.10.2020
+package fundamentos;
+
+public class PrimeiroPrograma {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!!! Primeiro Programa de novo!! Agora na Cod3r!");
+		
+	}
+
+}
