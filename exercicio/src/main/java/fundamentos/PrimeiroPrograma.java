d00085339f009bb6b094da0dfbd54045a2a57d53
jcgsr <cabralguerra@gmail.com>
2020-10-16 08:24:14 -0300

    desafio Temperatura

---
+++ exercicio/src/main/java/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+/*
+ *16.10.2020
+ */
+package fundamentos;
+
+/**
+ *
+ * @author js
+ */
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        System.out.println("Hello World!! De novo!! Agora na Coder!!");
+    }
+}
---
+++ exercicios/src/fundamentos/Temperatura.java
@@ -0,0 +1,21 @@
+// 16.10.2020
+package fundamentos;
+
+public class Temperatura {
+	public static void main(String[] args) {
+		final double X = 32;
+		final double Y = 5.0/9.0;
+		double f = 86;
+		double c = (f - X) * Y;
+		System.out.println("O resultado é " + c + "°C.");
+
+		f = 180;
+		c = (f - X) * Y;
+		System.out.println("O resultado é " + c + "°C.");
+		
+		
+	   
+		
+		
+	}
+}
